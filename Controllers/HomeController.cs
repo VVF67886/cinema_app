cd8dabd5c733a5aea98967887bd3fe6de23a4dda
Tommaso Tomasetti <t.tomasetti@outlook.com>
2024-10-01 16:25:25 +0200

    Home controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using CinemaApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HomeController : ControllerBase
+	{
+		private readonly CinemaContext _context;
+
+		public HomeController(CinemaContext context)
+		{
+			_context = context;
+		}
+
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<FilmHome>>> GetFilms()
+		{
+			return await _context.Films
+				.Where(x => x.Home != null)
+				.OrderBy(x => x.Home)
+				.Select(x=> new FilmHome(x))
+				.ToListAsync();
+		}
+
+	}
+}
---
+++ Models/FilmHome.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CinemaApi.Models;
+
+public partial class FilmHome
+{
+
+    public FilmHome(Film film) { 
+    
+        Id = film.Id;
+		Titolo = film.Titolo;
+		Immagine = film.Immagine;
+		Home = film.Home;
+	}
+
+    public int Id { get; set; }
+
+    public string? Titolo { get; set; }
+
+    public string? Immagine { get; set; }
+
+    public int? Home { get; set; }
+}
